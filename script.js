1a6f3a746590ec35b76d9e89a908edecd37205bd
luis-afonso136 <luis.manuel.afonso@ipvc.pt>
2024-07-03 23:38:57 +0100

    Profile-Page

---
+++ script.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+    var toggleSound = $("#toggleSound")[0];
+    var openSound = $("#openSound")[0];
+    var hoverSound = $("#hoverSound")[0];
+
+    let menuToggle = document.querySelector(".menuToggle");
+    let menu = document.querySelector(".menu");
+
+    menuToggle.onclick = function () {
+        menu.classList.toggle("active");
+        toggleSound.currentTime = 0; 
+        toggleSound.play();
+        if (menu.classList.contains("active")) {
+            openSound.currentTime = 0; 
+            openSound.play();
+        }
+    };
+
+    $("a").mouseenter(function() {
+        hoverSound.currentTime = 0; 
+        hoverSound.play();
+    });
+
+    particlesJS("particles", {
+        particles: {
+            number: {
+                value: 100,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: "#ffffff"
+            },
+            shape: {
+                type: "star",
+                stroke: {
+                    width: 0,
+                    color: "#000000"
+                },
+                polygon: {
+                    nb_sides: 5
+                }
+            },
+            opacity: {
+                value: 0.8,
+                random: true,
+                anim: {
+                    enable: true,
+                    speed: 1,
+                    opacity_min: 0,
+                    sync: false
+                }
+            },
+            size: {
+                value: 3,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false
+                }
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1
+            },
+            move: {
+                enable: true,
+                speed: 2,
+                direction: "none",
+                random: true,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200
+                }
+            }
+        },
+        interactivity: {
+            detectsOn: "canvas",
+            events: {
+                onHover: {
+                    enable: true,
+                    mode: "repulse"
+                },
+                onClick: {
+                    enable: true,
+                    mode: "push"
+                },
+                resize: true
+            },
+            modes: {
+                repulse: {
+                    distance: 100,
+                    duration: 0.4
+                },
+                push: {
+                    particles_nb: 4
+                }
+            }
+        },
+        retina_detect: true
+    });
+});
